8b3505441040a3c67ddf10ea8ca2f721f0237a09
Manu <emanuellybs14@gmail.com>
2023-06-23 11:19:49 -0300

    \

--- Back/Controllers/UserController.cs
+++ Back/Controllers/UserController.cs
@@ -1,24 +1,28 @@
-// using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using 
+
+using Back.Model;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Back.Controllers;
+
+[Authorize]
+[Route("api/user")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    // [HttpPost("/")]
+
+    // public async Task<ActionResult<UserBaddit>> NewUser(
+    //     [FromServices] UserBaddit userRep,
+    //     [FromServices] IPasswordHasher psh,
+    //     [FromServices] ISaltProvider slp,
+    //     [FromBody] UserRegister registerData)
+    //     [FromBody] IRepository<UserBaddit> repo
+    // )
+    // {
+
+    // }
 
 
-// using Back.Model;
-
-// namespace Back.Controllers;
-
-// [ApiController]
-// [Route("[controller]")]
-// public class UserController : ControllerBase
-// {
-//     [HttpPost("/")]
-
-//     public async Task<ActionResult<UserBaddit>> NewUser(
-//         [FromServices] UserBaddit userRep,
-//         [FromServices] IPasswordHasher psh,
-//         [FromServices] ISaltProvider slp,
-//         [FromBody] UserRegister registerData)
-//         [FromBody] IRepository<UserBaddit> repo
-//     )
-//     {
-
-//     }
-// }
+}
--- Back/Program.cs
+++ Back/Program.cs
@@ -1,17 +1,41 @@
 using Back.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+using Back.Repositories.User;
+
 // using Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
+
 builder.Services.AddCors(opt =>
 {
-   opt.AddPolicy("MainPolicy", cors =>
-   {
+    opt.AddPolicy("MainPolicy", cors =>
+    {
         cors
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowAnyOrigin();
-   });
+           .AllowAnyHeader()
+           .AllowAnyMethod()
+           .AllowAnyOrigin();
+    });
 });
 
 builder.Services.AddScoped<BadditContext>();
@@ -21,8 +45,6 @@ builder.Services.AddTransient<IRepository<UserBaddit>, UserRepository>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
