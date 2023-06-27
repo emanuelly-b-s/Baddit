e49df8c4ce115783ea2cd35db1916b190952c6aa
Emanuelly B. Silva <emanuellybs14@gmail.com>
2023-06-27 17:06:04 -0300

    testando

--- Front/src/app/pages/newaccount/newaccount.component.ts
+++ Front/src/app/pages/newaccount/newaccount.component.ts
@@ -1,7 +1,7 @@
 // import { UserService } from './../services/users.service';
 // import { NewAccountService } from './../../services/new-account.service';
 import { Component, EventEmitter, Output } from '@angular/core';
-import { Validators, FormControl } from '@angular/forms';
+import { Validators, FormControl, FormGroup } from '@angular/forms';
 import { UserService } from '../../services/users.service';
 import { User } from '../../interfaces/User';
 import { Router } from '@angular/router';
@@ -14,8 +14,56 @@ import { Router } from '@angular/router';
 })
 export class NewaccountComponent {
 
-  constructor(private router : Router, private userService : UserService) { }
+  constructor(private router : Router, private userService : UserService, private form : FormGroup, private _fb : FormGroup
+    ) { }
 
+ 
+
+  ngOnit(){
+    this.register();
+  }
+  
+  formUser()
+  {
+    this.form =  new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email,
+        Validators.minLength(4),
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+      ]),
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+      ]),
+      lastname: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+      ]),
+      datebirth : new FormControl('', []),
+      nickusername: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8)
+      ]),
+      password : new FormControl('', [
+        Validators.required,
+        Validators.minLength(8)
+      ]),
+      
+    })
+  }
+
+  userRegister : User =
+  {
+    email : "",
+    username : "",
+    lastname : "",
+    datebirth : new Date(),
+    nickuser : "",
+    passworduser : "",
+    saldpassword: "",
+    photouser : ""
+  }
 
   email = new FormControl('', [
     Validators.required,
@@ -33,7 +81,7 @@ export class NewaccountComponent {
     Validators.required,
     Validators.minLength(8)
   ]);
-
+  
 
   // this.myGroup(
 
@@ -51,17 +99,6 @@ export class NewaccountComponent {
   //     firstName: new FormControl()
   // });
 
-    userRegister : User =
-    {
-      email : "",
-      username : "",
-      lastname : "",
-      datebirth : new Date(),
-      nickuser : "",
-      passworduser : "",
-      saldpassword: "",
-      photouser : ""
-    }
 
     passwordChanged(newPass: string)
     {
